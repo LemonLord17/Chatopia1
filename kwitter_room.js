ff9f19c368302e8c8e0ab5fc1eab471c6fa4a928
LemonLord17 <77705001+LemonLord17@users.noreply.github.com>
2021-11-20 13:28:25 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPNNkzB1LRSQyD2ir0Qbb-lPNAjC3F-z8",
+    authDomain: "kwitter-329b6.firebaseapp.com",
+    databaseURL: "https://kwitter-329b6-default-rtdb.firebaseio.com",
+    projectId: "kwitter-329b6",
+    storageBucket: "kwitter-329b6.appspot.com",
+    messagingSenderId: "920736815218",
+    appId: "1:920736815218:web:44bc7382eebc8d62f48aeb"
+  };
